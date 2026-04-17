737ae13f1e1a2b85fec40554e7d1854548caa67d
sg9924 <sg9924@users.noreply.github.com>
2026-04-17 22:43:46 +0530

    Method 1 | Version 3 | Cleanup

--- Method 1 - Normal/version 3/7seg_single_digit_test.c
+++ Method 1 - Normal/version 3/7seg_single_digit_test.c
@@ -9,19 +9,19 @@ int main()
     board_init();
 
     segments_t seg_pins = {GPIO_PIN7, GPIO_PIN9, GPIO_PIN0, GPIO_PIN5, GPIO_PIN10, GPIO_PIN8, GPIO_PIN4, GPIO_PIN1};
-    seg_pin_configure(&seg_pins, GPIOA);
+    digits_t digit_pins = {GPIO_PIN5, GPIO_PIN6, GPIO_PIN7, GPIO_PIN8};
+
+    seg_configure(&seg_pins, GPIOA, &digit_pins, GPIOB);
     seg_init();
-    seg_pins_reset();
-    
+    seg_digit_select(digit_pins.DIGIT_1);
+
     while(1)
 	{
-        
         for(uint8_t c='0'; c<='9'; c++)
         {
             seg_display(c, WITH_DECIMAL);
             Systick_delay(1000);
-        }
-        
+        }        
     }
     return 0;
 }
